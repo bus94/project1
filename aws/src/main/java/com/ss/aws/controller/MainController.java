a8643564c5e134c4ed76a0be1b0653e71a3e8eb0
bus94 <lbs927@naver.com>
2024-10-11 10:20:58 +0900

    aws

---
+++ aws/src/main/java/com/ss/aws/controller/MainController.java
@@ -0,0 +1,35 @@
+package com.ss.aws.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class MainController {
+	// 고정 IP: 3.38.70.195
+	
+	@GetMapping("/aws/v1") // ?number=0 등의 변수를 받은 것을 로그값 찍을 예쩡
+	public String main(@RequestParam(defaultValue = "1") Integer number) {
+		if(number == 1) {
+			log.info("/aws/v1 호출 됩니다! info 로그################");
+		} else if(number == -1) {
+			log.error("/aws/v1 호출 됩니다! error 로그################");
+		} else if(number == 0) {
+			log.warn("/aws/v1 호출 됩니다! warn 로그################");
+		}
+		return "<h1> aws V1 </h1>";
+	}
+}
+
+/*
+ * 1. 배포 위치 EC2 기존 만들었던 서버 모두 인스턴스 종료!
+ * 	  AWS 프리티어 사용자 (회원 가입한 지 1년이 안된 고객)
+ * 		  750시간(약 한 달) , 30GB 까지 무료로 사용
+ *        유동적인 IPv4 → 고정적인 IP를 무료로 사용할 수 있도록 조건
+ *        
+ *  
+ */
+
