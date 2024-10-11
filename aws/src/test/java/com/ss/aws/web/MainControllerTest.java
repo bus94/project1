8f06ca5ddfa59b3cfa57a177f7aef786629c7e13
bus94 <lbs927@naver.com>
2024-10-11 11:13:48 +0900

    gradle 추가

--- aws/src/test/java/com/ss/aws/web/MainControllerTest.java
+++ aws/src/test/java/com/ss/aws/web/MainControllerTest.java
@@ -5,10 +5,11 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.ResponseEntity;
 
-@SpringBootTest(webEnvironment = )
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 public class MainControllerTest {
 	@Autowired
 	private TestRestTemplate rest;
